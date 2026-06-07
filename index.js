7ce102f93b1d19842528464fe1a898ef623b3ef1
Muhammad Hussein <muhammadrides@gmail.com>
2026-06-06 22:19:42 -0500

    1st commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/mhdsb-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/mhdsb-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/mhdsb-ping - Check bot latency
+/mhdsb-catfact - Get a cat fact`
+  });
+});
+
+app.command("/mhdsb-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact"); //Fetches cat fact from free apis
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
